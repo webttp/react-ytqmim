818033918fc4ab789c46205f0b671cb0c0020d73
webttp <smartin.black@gmail.com>
2020-01-03 17:32:09 +0530

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          Start editing tokk see some magic happen :)
         </p>
       </div>
     );
